8429a9473c427e69a6b059472d328f8fa0f2f970
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-05-01 17:53:06 -0500

    Adding registration form

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
+from bookmarks.forms import *
 
 def main_page(request): 
   return render_to_response( 
@@ -30,4 +31,26 @@ def user_page(request, username):
 def logout_page(request): 
   logout(request) 
   return HttpResponseRedirect('/') 
+
+def register_page(request): 
+  if request.method == 'POST': 
+    form = RegistrationForm(request.POST) 
+    if form.is_valid(): 
+      user = User.objects.create_user( 
+        username=form.clean_data['username'], 
+        password=form.clean_data['password1'], 
+        email=form.clean_data['email'] 
+      ) 
+      return HttpResponseRedirect('/register/success/') 
+  else: 
+    form = RegistrationForm() 
     
+  variables = RequestContext(request, { 
+     'form': form 
+  })
+   
+  return render_to_response( 
+     'registration/register.html',  
+     variables 
+   ) 
+     
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from bookmarks.views import *
 import os.path
+from django.views.generic.simple import direct_to_template 
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,8 +16,11 @@ urlpatterns = patterns('',
     (r'^user/(\w+)/$', user_page),
     (r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', logout_page),
-    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', 
-       { 'document_root': site_media }),
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': site_media }),
+    (r'^register/$', register_page),
+    (r'^register/success/$', direct_to_template, 
+      { 'template': 'registration/register_success.html' }),
+      
     # Example:
     # (r'^django_bookmarks/', include('django_bookmarks.foo.urls')),
 
