a390ee9321556a43589d3b45b00a39270735082e
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-05-01 01:47:12 -0500

    Adding first template

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,17 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse 
+from django.template import Context 
+from django.template.loader import get_template 
 
 def main_page(request): 
-  output = ''' 
-    <html> 
-      <head><title>%s</title></head> 
-      <body> 
-        <h1>%s</h1><p>%s</p> 
-      </body> 
-    </html> 
-  ''' % ( 
-    'Django Bookmarks', 
-    'Welcome to Django Bookmarks', 
-    'Where you can store and share bookmarks!' 
-  ) 
+  template = get_template('main_page.html') 
+  variables = Context({ 
+    'head_title': 'Django Bookmarks', 
+    'page_title': 'Welcome to Django Bookmarks', 
+    'page_body': 'Where you can store and share bookmarks!' 
+  }) 
+  output = template.render(variables) 
   return HttpResponse(output)
