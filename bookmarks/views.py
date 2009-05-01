d1c2671827f6f5a1edb49ff775bf2666f3e3cc4d
Norberto Ortigoza <norberto.ortigoza@gmail.com>
2009-05-01 14:32:39 -0500

    Adding template

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -4,17 +4,13 @@ from django.template import Context
 from django.template.loader import get_template 
 from django.http import HttpResponse, Http404 
 from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
 
 def main_page(request): 
-    template = get_template('main_page.html') 
-    variables = Context({ 
-    'head_title': 'Django Bookmarks', 
-    'page_title': 'Welcome to Django Bookmarks', 
-    'page_body': 'Where you can store and share bookmarks!' 
-    }) 
-    output = template.render(variables) 
-    return HttpResponse(output)
-  
+  return render_to_response( 
+    'main_page.html', 
+    { 'user': request.user } 
+  ) 
   
 def user_page(request, username): 
     try: 
